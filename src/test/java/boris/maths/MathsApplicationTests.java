d91e79a04282c0f1a820b108f2e838dc475c3c4d
NilayDas <mailnilaydas@gmail.com>
2023-10-05 19:42:15 +0530

    1st commit

---
+++ src/test/java/boris/maths/MathsApplicationTests.java
@@ -0,0 +1,14 @@
+package boris.maths;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MathsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+
+
+}
